d3f98584e410a15891e58be2c96bd5dfd8340784
brsaca <brsaca@gmail.com>
2023-10-02 17:37:24 -0600

    Initial Commit

---
+++ BooksUI/BooksUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksUIApp.swift
+//  BooksUI
+//
+//  Created by Brenda Saavedra Cantu on 02/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BooksUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
